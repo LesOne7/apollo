9106f5544e74f5c4c45496e1786908b78c6a7254
LesOne7 <55252052+LesOne7@users.noreply.github.com>
2020-05-06 22:51:58 +0300

    introspecrion and playground

--- src/index.js
+++ src/index.js
@@ -25,6 +25,8 @@ const server = new ApolloServer({
     },
     typeDefs,
     resolvers,
+    introspection: true,
+    playground: true,
     dataSources: () => ({
         launchAPI: new LaunchAPI(),
         userAPI: new UserAPI({ store })
